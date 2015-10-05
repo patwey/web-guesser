849614885916fbad5fae33336fdc208ee55bb86a
Pat Wey <patrickwey@gmail.com>
2015-10-05 12:41:04 -0400

    it's complete!

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+set :number, rand(100)
+
+def check_guess(guess)
+  if guess > settings.number + 5
+    message = 'Way too high!'
+  elsif guess < settings.number - 5
+    message = 'Way too low!'
+  elsif guess < settings.number
+    message = 'Too low!'
+  elsif guess > settings.number
+    message = 'Too high!'
+  else
+    message = 'You got it right!'
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params['guess'].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => settings.number, :message => message}
   # pass a hash in with the variable name as a sym pointing to its value
 end
